b70d62efd697e8875344b49117b848456efbf335
BIBI78 <blakecollins78@gmail.com>
2022-10-10 17:31:18 +0000

    edit to h3 output

--- assets/js/1player.js
+++ assets/js/1player.js
@@ -488,30 +488,29 @@ d3.addEventListener('animationend', removeSpinClass);
   
  if(compareValueOne ===1000)  {
   console.log("automatic win player 1");
-  document.querySelector('h1').innerHTML = 'Automatic Win PLAYER 1'
+  document.querySelector('h3').innerHTML = 'Automatic Win PLAYER 1'
   
   
   } else if (compareValueOne === 0){
   console.log("automatic lose player 1");
-  document.querySelector('h1').innerHTML = 'Automatic Lose PLAYER 1'
+  document.querySelector('h3').innerHTML = 'Automatic Lose PLAYER 1'
 
 } else if (myRollOne[0] === myRollOne[1] && myRollOne[1] === myRollOne[2]){
 
-  document.querySelector('h1').innerHTML = 'Tripple' 
+  document.querySelector('h3').innerHTML = 'Tripple' 
   
   }else if (myRollOne[1] === myRollOne[2]){
-    document.querySelector('h1').innerHTML = 'Snake Eyes'
+    document.querySelector('h3').innerHTML = 'Snake Eyes'
 
   }else if (myRollOne[1] === myRollOne[0]){
-    document.querySelector('h1').innerHTML = 'Snake Eyes'
+    document.querySelector('h3').innerHTML = 'Snake Eyes'
 
   }else if (myRollOne[0] === myRollOne[2]){
-    document.querySelector('h1').innerHTML = 'Snake Eyes'
-
-  //??????? why does this keep coming up?  
+    document.querySelector('h3').innerHTML = 'Snake Eyes'
+ 
   } else {
-  console.log("no idea what case this is")
-  document.querySelector('h1').innerHTML = 'Snake Eyes'
+  console.log("final case")
+  document.querySelector('h3').innerHTML = 'Snake Eyes'
 
 
 
